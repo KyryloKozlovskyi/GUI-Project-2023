a7636f4a5242c9b32e7c6d8eaeec6cdd34b254c6
AdamGallagher339 <127843896+AdamGallagher339@users.noreply.github.com>
2023-05-04 20:39:09 +0100

    uodated c&c page

--- js/Main.js
+++ js/Main.js
@@ -4,6 +4,12 @@ let dish1quant = 0;
 let dish2quant = 0;
 let dish3quant = 0;
 let dish4quant = 0;
+let dish5quant = 0;
+let dish6quant = 0;
+let dish7quant = 0;
+let dish8quant = 0;
+let dish9quant = 0;
+let dish10quant = 0;
 let cost = 0;
 let total = 0;
 let printTime = 0;
@@ -48,9 +54,63 @@ function item_remove_dish4(){
     document.getElementById("item4").innerHTML = dish4quant;
 }
 
+function item_add_dish5(){
+    dish5quant = dish5quant + 1;
+    document.getElementById("item5").innerHTML = dish5quant;
+}
+function item_remove_dish5(){
+    if(dish5quant > 0){dish5quant = dish5quant - 1;}
+    document.getElementById("item5").innerHTML = dish5quant;
+}
+
+function item_add_dish6(){
+    dish6quant = dish6quant + 1;
+    document.getElementById("item6").innerHTML = dish6quant;
+}
+function item_remove_dish6(){
+    if(dish6quant > 0){dish6quant = dish6quant - 1;}
+    document.getElementById("item6").innerHTML = dish6quant;
+}
+
+function item_add_dish7(){
+    dish7quant = dish7quant + 1;
+    document.getElementById("item7").innerHTML = dish7quant;
+}
+function item_remove_dish7(){
+    if(dish7quant > 0){dish7quant = dish7quant - 1;}
+    document.getElementById("item7").innerHTML = dish7quant;
+}
+
+function item_add_dish8(){
+    dish8quant = dish8quant + 1;
+    document.getElementById("item8").innerHTML = dish8quant;
+}
+function item_remove_dish8(){
+    if(dish8quant > 0){dish8quant = dish8quant - 1;}
+    document.getElementById("item8").innerHTML = dish8quant;
+}
+
+function item_add_dish9(){
+    dish9quant = dish9quant + 1;
+    document.getElementById("item9").innerHTML = dish9quant;
+}
+function item_remove_dish9(){
+    if(dish9quant > 0){dish9quant = dish9quant - 1;}
+    document.getElementById("item9").innerHTML = dish9quant;
+}
+
+function item_add_dish10(){
+    dish10quant = dish10quant + 1;
+    document.getElementById("item10").innerHTML = dish10quant;
+}
+function item_remove_dish10(){
+    if(dish10quant > 0){dish10quant = dish10quant - 1;}
+    document.getElementById("item10").innerHTML = dish10quant;
+}
+
 function update_cost(){
-    total = dish1quant*5 + dish2quant*5 + dish3quant*5 + dish4quant*5;
-    cost = total;
+    total = dish1quant*2.99 + dish2quant*5.99 + dish3quant*3.99 + dish4quant*4.99 + dish5quant*3.99 + dish6quant*2.99 + dish7quant*2.50 + dish8quant*2 + dish9quant*2.50 + dish10quant*3.20;
+    cost = Math.round((total + Number.EPSILON) * 100) / 100;
     document.getElementById("Ammount").innerHTML = "€"+cost;
 }
 
@@ -61,10 +121,10 @@ function payment(){
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date+' '+time;
         printTime = (dateTime);
-        finishTime = today.getHours() + ":" + (today.getMinutes()+30) + ":" + today.getSeconds();
+        finishTime = (today.getHours()+1) + ":" + today.getMinutes() + ":" + today.getSeconds();
 
         document.getElementById("orderNum").innerHTML = Math.floor(Math.random() * 1000) + Math.floor(Math.random() * 1000);
-        document.getElementById("price-of").innerHTML = total;
+        document.getElementById("price-of").innerHTML = cost;
         document.getElementById("print").innerHTML = printTime;
         document.getElementById("finish").innerHTML = finishTime;
 
@@ -74,3 +134,40 @@ function payment(){
 }
 
 // END OF CHECKOUT JS //
+
+
+
+
+
+
+// Soup of the day by Fionn McCarthy
+
+let curDate = new Date();   //  Reads in current date from browser
+let day = curDate.getDay(); //  Reads in current day
+var mealOfDay;
+
+switch (day) {  //  Decides Soup of the Day
+    case 0:
+        mealOfDay = "Tomato and Basil Soup";    //  Soup description
+    break;
+    case 1:
+        mealOfDay = "Seafood Chowder";
+    break;
+    case 2:
+        mealOfDay = "Leek and Potato Soup";
+    break;
+    case 3:
+        mealOfDay = "Chorizo and Bean Soup";
+    break;
+    case 4:
+        mealOfDay = "Butternut Squash and Chilli Soup";
+        break;
+    case 5:
+        mealOfDay = "Carrot and Coriander Soup";
+    break;
+    default:
+        mealOfDay = "Potato and Leek Soup";
+}
+
+//  Text outputted
+document.getElementById("SoupOfTheDay").innerHTML = ("Todays soup of the day is: <br>"+"<b>"+mealOfDay)
