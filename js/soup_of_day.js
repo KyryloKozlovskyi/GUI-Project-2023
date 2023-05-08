bb6b13a087250d1fc26b60a274d4ff77566b4d32
AdamGallagher339 <127843896+AdamGallagher339@users.noreply.github.com>
2023-05-08 18:49:29 +0100

    final touches

--- js/soup_of_day.js
+++ js/soup_of_day.js
@@ -8,32 +8,32 @@ switch (day) {  //  Decides Soup of the Day
     case 0:
         mealOfDay = "Tomato and Basil Soup";    //  Soup description
         //  Image src
-        document.getElementById("soup").style.backgroundImage = 'url(../images/soup/sunday.jpg)';
+        document.getElementById("soup").style.backgroundImage = 'url(images/soup/sunday.jpg)';
     break;
     case 1:
         mealOfDay = "Seafood Chowder";
-        document.getElementById("soup").style.backgroundImage = 'url(../images/soup/monday.jpg)';
+        document.getElementById("soup").style.backgroundImage = 'url(images/soup/monday.jpg)';
 
     break;
     case 2:
         mealOfDay = "Leek and Potato Soup";
-        document.getElementById("soup").style.backgroundImage = 'url(../images/soup/tuesday.jpg)';
+        document.getElementById("soup").style.backgroundImage = 'url(images/soup/tuesday.jpg)';
     break;
     case 3:
         mealOfDay = "Chorizo and Bean Soup";
-        document.getElementById("soup").style.backgroundImage = 'url(../images/soup/wednesday.png)';
+        document.getElementById("soup").style.backgroundImage = 'url(images/soup/wednesday.png)';
     break;
     case 4:
         mealOfDay = "Butternut Squash and Chilli Soup";
-        document.getElementById("soup").style.backgroundImage = 'url(../images/soup/thursday.jpg)';
+        document.getElementById("soup").style.backgroundImage = 'url(images/soup/thursday.jpg)';
         break;
     case 5:
         mealOfDay = "Carrot and Coriander Soup";
-        document.getElementById("soup").style.backgroundImage = 'url(../images/soup/friday.jpg)';
+        document.getElementById("soup").style.backgroundImage = 'url(images/soup/friday.jpg)';
     break;
     default:
         mealOfDay = "Potato and Leek Soup";
-        document.getElementById("soup").style.backgroundImage = 'url(../images/soup/saturday.jpg)';
+        document.getElementById("soup").style.backgroundImage = 'url(images/soup/saturday.jpg)';
 }
 
 //  Text outputted
